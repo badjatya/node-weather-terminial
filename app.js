189a5f402fe22d9f4c9891ecb9da5698ff5fa097
Archit Badjatya <architj240@gmail.com>
2021-08-31 16:27:28 +0530

    Fetching cordinates

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const axios = require("axios");
+const weatherUrl =
+  "http://api.weatherstack.com/current?access_key=e97b1fccb1d8615cf7726d19141a4745&query=indore";
+const mapBoxUrl =
+  "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiYmFkamF0eWEiLCJhIjoiY2tzenk4NGRnMnh1NDJ3b2QzejAxNzlvYyJ9.m3DvRsBldnIG4hLTIVfG8A";
 
 const getLocation = async () => {
   try {
-    const response = await axios.get(
-      "http://api.weatherstack.com/current?access_key=e97b1fccb1d8615cf7726d19141a4745&query=indore"
-    );
+    const response = await axios.get(weatherUrl);
     const data = response.data.current;
     console.log(
       `It is currently ${data.temperature} degrees out. There is a ${data.precip}% chance of rain`
@@ -14,4 +16,16 @@ const getLocation = async () => {
   }
 };
 
-getLocation();
+const getGeoCoding = async () => {
+  try {
+    const response = await axios.get(mapBoxUrl);
+    const data = response.data.features[0].geometry.coordinates;
+    console.log("Latitude: " + data[0]);
+    console.log("Longitude: " + data[1]);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// getLocation();
+getGeoCoding();
