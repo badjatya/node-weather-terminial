7d0c5f01c80ba4b2ce9ee30b775b6970dd62bd16
Archit Badjatya <architj240@gmail.com>
2021-08-31 21:13:07 +0530

    weather app bugs fixed

--- app.js
+++ app.js
@@ -1,31 +1,28 @@
-const axios = require("axios");
+const request = require("request");
 const weatherUrl =
   "http://api.weatherstack.com/current?access_key=e97b1fccb1d8615cf7726d19141a4745&query=indore";
-const mapBoxUrl =
+
+// request({ url: url, json: true }, (error, response) => {
+//     if (error) {
+//         console.log('Unable to connect to weather service!')
+//     } else if (response.body.error) {
+//         console.log('Unable to find location')
+//     } else {
+//         console.log(response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degress out. There is a ' + response.body.currently.precipProbability + '% chance of rain.')
+//     }
+// })
+
+const geocodeURL =
   "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiYmFkamF0eWEiLCJhIjoiY2tzenk4NGRnMnh1NDJ3b2QzejAxNzlvYyJ9.m3DvRsBldnIG4hLTIVfG8A";
 
-const getLocation = async () => {
-  try {
-    const response = await axios.get(weatherUrl);
-    const data = response.data.current;
-    console.log(
-      `It is currently ${data.temperature} degrees out. There is a ${data.precip}% chance of rain`
-    );
-  } catch (error) {
-    console.error(error);
+request({ url: geocodeURL, json: true }, (error, response) => {
+  if (error) {
+    console.log("Unable to connect to location services!");
+  } else if (response.body.features.length === 0) {
+    console.log("Unable to find location. Try another search.");
+  } else {
+    const latitude = response.body.features[0].center[0];
+    const longitude = response.body.features[0].center[1];
+    console.log(latitude, longitude);
   }
-};
-
-const getGeoCoding = async () => {
-  try {
-    const response = await axios.get(mapBoxUrl);
-    const data = response.data.features[0].geometry.coordinates;
-    console.log("Latitude: " + data[0]);
-    console.log("Longitude: " + data[1]);
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-// getLocation();
-getGeoCoding();
+});
