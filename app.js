32019cecb2f88ba5664475cf1cfcd479f0dcf6e0
Archit Badjatya <architj240@gmail.com>
2021-08-31 11:58:35 +0530

    fetching data

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ const getLocation = async () => {
     const response = await axios.get(
       "http://api.weatherstack.com/current?access_key=e97b1fccb1d8615cf7726d19141a4745&query=indore"
     );
-    // const data = response.json();
-    console.log(response.data);
+    const data = response.data.current;
+    console.log(
+      `It is currently ${data.temperature} degrees out. There is a ${data.precip}% chance of rain`
+    );
   } catch (error) {
     console.error(error);
   }
