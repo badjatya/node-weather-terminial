3a88dbacbeab63f06431182463fcd93e5325af3a
Archit Badjatya <architj240@gmail.com>
2021-09-01 15:30:36 +0530

    forecast utils added for weather api

--- app.js
+++ app.js
@@ -1,20 +1,14 @@
-const request = require("request");
 const geoCode = require("./utils/geoCode");
+const forecast = require("./utils/forecast");
 
-// const weatherUrl =
-//   "http://api.weatherstack.com/current?access_key=e97b1fccb1d8615cf7726d19141a4745&query=indore";
+// geoCode("Indore", (error, data) => {
+//   console.log(
+//     "Error: " + error + "\nData: " + data.latitude + "\n " + data.longitude
+//   );
+//   console.log("--------------");
+// });
 
-// request({ url: url, json: true }, (error, response) => {
-//     if (error) {
-//         console.log('Unable to connect to weather service!')
-//     } else if (response.body.error) {
-//         console.log('Unable to find location')
-//     } else {
-//         console.log(response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degress out. There is a ' + response.body.currently.precipProbability + '% chance of rain.')
-//     }
-// })
-
-geoCode("Indore", (error, data) => {
-  console.log("Error: " + error + "\nData: " + data.latitude);
-  console.log("--------------");
+forecast(44.1545, -75.7088, (error, data) => {
+  console.log("Error", error);
+  console.log("Data", data);
 });
